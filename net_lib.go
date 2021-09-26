c7d68251e1de4edbc7dc9bae388d18a49eadcdae
mian <38103855+intmian@users.noreply.github.com>
2021-09-26 11:28:03 +0800

    优化一下配置

--- net_lib.go
+++ net_lib.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math/rand"
 	"net/http"
 	"net/url"
 	"time"
@@ -27,6 +28,8 @@ func (h *NetHttping) Init() {
 func (h *NetHttping) Httping(url string, useProxy bool, result chan<- int) {
 	go func() {  // 为了避免阻塞Httping，所以在此处起一个
 		h.maxChan <- 1 // 用channel做并发控制
+		randT := rand.Uint64() % uint64( gSetting.Data.HttpRequestRandTimeOutMillisecond) // 避免阻塞做一个随机延迟
+		time.Sleep(time.Millisecond * time.Duration(randT))
 		result <- h.httping(url, useProxy)
 		<-h.maxChan
 	}()
@@ -57,6 +60,6 @@ func buildHTTPClient(isProxy bool) *http.Client {
 		}
 	}
 	transport := &http.Transport{Proxy: proxy}
-	client := &http.Client{Transport: transport,Timeout: time.Second * 5}  // 暂定三秒，避免有些注定收不到的请求完全占用了多个线程而卡住了
+	client := &http.Client{Transport: transport,Timeout: time.Second * time.Duration(gSetting.Data.HttpTimeOutSecond)}  // 暂定三秒，避免有些注定收不到的请求完全占用了多个线程而卡住了
 	return client
 }
--- setting.go
+++ setting.go
@@ -3,13 +3,15 @@ package main
 import "net_detect/misc"
 
 type SettingJson struct {
-	Proxy       string   `json:"proxy"`         // 代理端口
-	WebCheckNum int      `json:"web_check_num"` // 单个网页的测试次数
-	MaxParallel int      `json:"max_parallel"`  // 最大测试并发，过大会因为阻塞测不准
-	Websites    []string `json:"websites"`      // 自定义的一些网页
-	WebChina        string   `json:"web_china"`
-	WebForeignUnban string   `json:"web_foreign_unban"`
-	WebForeignBan   string   `json:"web_foreign_ban"`
+	Proxy                             string   `json:"proxy"`         // 代理
+	WebCheckNum                       int      `json:"web_check_num"` // 单个网页的测试次数
+	MaxParallel                       int      `json:"max_parallel"`  // 最大测试并发，过大会因为阻塞测不准
+	Websites                          []string `json:"websites"`      // 自定义的一些网页
+	WebChina                          string   `json:"web_china"`
+	WebForeignUnban                   string   `json:"web_foreign_unban"`
+	WebForeignBan                     string   `json:"web_foreign_ban"`
+	HttpTimeOutSecond                 int      `json:"http_time_out_second"` // http client 设置的timeout
+	HttpRequestRandTimeOutMillisecond int      `json:"http_request_rand_time_out_millisecond"`  // http client 发送请求的随机延迟
 }
 
 type Setting struct {
